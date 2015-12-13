85ca866e175de6ec1376708497f9b1a2e45ea32c
Olaf Peters <olaf@guesswhapp.de>
2015-12-13 15:45:26 +0100

    Adjusted Tab Size to 2

--- controllers/home.js
+++ controllers/home.js
@@ -1,15 +1,15 @@
 module.exports = {
-    registerRoutes: function(app) {
-        app.get('/', this.home);
-        app.get('/test', this.test);
-    },
+  registerRoutes: function(app) {
+    app.get('/', this.home);
+    app.get('/test', this.test);
+  },
 
-    home: function(req, res) {
-        res.render('home', { title: 'Express', texto: 'olaf' });
-    },
+  home: function(req, res) {
+    res.render('home', { title: 'Express', texto: 'olaf' });
+  },
 
-    test: function (req, res, next) {
-    	res.render('home', { title: 'Testeando', texto: 'olaf2' });
-    },
+  test: function (req, res, next) {
+    res.render('home', { title: 'Testeando', texto: 'olaf2' });
+  },
 
 };
